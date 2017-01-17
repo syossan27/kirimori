1215af936aa556033056d0fec4dd355e4044f7df
syossan27 <wisdom1027@gmail.com>
2017-01-17 20:54:40 +0900

    Refactoring use AST

--- kirimori.go
+++ kirimori.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/BurntSushi/toml"
+	vimlparser "github.com/haya14busa/go-vimlparser"
+	"github.com/haya14busa/go-vimlparser/ast"
 	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli"
 )
@@ -19,6 +22,175 @@ const (
 type Config struct {
 	VimrcPath   string
 	ManagerType string
+}
+
+var opt = &vimlparser.ParseOption{}
+
+var addPlugin = false
+var removePlugin = false
+var installPlugins []string
+var addLine int
+var removeLine int
+var plugin_name string
+
+type AddVundleVisitor struct {
+}
+
+func (v AddVundleVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Excmd:
+			if n.Cmd().Name == "Bundle" {
+				addLine = n.Pos().Line
+			}
+		}
+	}
+	return v
+}
+
+type RemoveVundleVisitor struct {
+}
+
+func (v RemoveVundleVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Excmd:
+			if n.Cmd().Name == "Bundle" {
+				if strings.Contains(n.Command, plugin_name) {
+					removeLine = n.Pos().Line
+				}
+			}
+		}
+	}
+	return v
+}
+
+type ListVundleVisitor struct {
+}
+
+func (v ListVundleVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Excmd:
+			if n.Cmd().Name == "Bundle" {
+				command := n.Command
+				start := n.ExArg.Argpos.Offset - n.ExArg.Cmdpos.Offset
+				end := utf8.RuneCountInString(n.Command)
+				plugin_name = strings.Replace(command[start:end], "'", "", -1)
+				installPlugins = append(installPlugins, plugin_name)
+			}
+		}
+	}
+	return v
+}
+
+type AddNeoBundleVisitor struct {
+}
+
+func (v AddNeoBundleVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Excmd:
+			if n.Cmd().Name == "NeoBundle" {
+				addLine = n.Pos().Line
+			}
+		}
+	}
+	return v
+}
+
+type RemoveNeoBundleVisitor struct {
+}
+
+func (v RemoveNeoBundleVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Excmd:
+			if n.Cmd().Name == "NeoBundle" {
+				if strings.Contains(n.Command, plugin_name) {
+					removeLine = n.Pos().Line
+				}
+			}
+		}
+	}
+	return v
+}
+
+type ListNeoBundleVisitor struct {
+}
+
+func (v ListNeoBundleVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Excmd:
+			if n.Cmd().Name == "NeoBundle" {
+				command := n.Command
+				start := n.ExArg.Argpos.Offset - n.ExArg.Cmdpos.Offset
+				end := utf8.RuneCountInString(n.Command)
+				plugin_name = strings.Replace(command[start:end], "'", "", -1)
+				installPlugins = append(installPlugins, plugin_name)
+			}
+		}
+	}
+	return v
+}
+
+type AddDeinVisitor struct {
+}
+
+func (v AddDeinVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Ident:
+			if n.Name == "dein#add" {
+				addLine = n.Pos().Line
+			}
+		}
+	}
+	return v
+}
+
+type RemoveDeinVisitor struct {
+}
+
+func (v RemoveDeinVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Ident:
+			if n.Name == "dein#add" {
+				removePlugin = true
+			}
+		case *ast.BasicLit:
+			if removePlugin {
+				if strings.Contains(n.Value, plugin_name) {
+					removeLine = n.Pos().Line
+					removePlugin = false
+				}
+			}
+		}
+	}
+	return v
+}
+
+type ListDeinVisitor struct {
+}
+
+func (v ListDeinVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.Ident:
+			if n.Name == "dein#add" {
+				addPlugin = true
+			}
+		case *ast.BasicLit:
+			if addPlugin {
+				plugin_name = strings.Replace(n.Value, "'", "", -1)
+				installPlugins = append(installPlugins, plugin_name)
+				addPlugin = false
+			}
+		}
+	}
+	return v
 }
 
 func makeApp() *cli.App {
@@ -88,7 +260,7 @@ func makeApp() *cli.App {
 			Usage:   "add plugin",
 			Action: func(c *cli.Context) error {
 				// 設定ファイルの読み込み
-				var plugin_name = c.Args().First()
+				plugin_name = c.Args().First()
 				var conf Config
 				if _, err := toml.DecodeFile(setting_file_path, &conf); err != nil {
 					println("Error: Can't read setting file.")
@@ -107,16 +279,50 @@ func makeApp() *cli.App {
 
 					switch conf.ManagerType {
 					case "Vundle":
-						if err := addPluginForVundle(vimrc_file, plugin_name); err != nil {
+						scanAddLineForVundle(vimrc_file)
+
+						_, err := vimrc_file.Seek(0, 0)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail change file offset.\n")
+							os.Exit(ExitCodeError)
+						}
+
+						vimrc_content, err := createAddPluginContentForVundle(vimrc_file, plugin_name)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+							os.Exit(ExitCodeError)
+						}
+						if err := updateVimrc(conf.VimrcPath, vimrc_content); err != nil {
 							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail add plugin.\n")
 							os.Exit(ExitCodeError)
 						}
 					case "NeoBundle":
-						if err := addPluginForNeoBundle(vimrc_file, plugin_name); err != nil {
+						scanAddLineForNeoBundle(vimrc_file)
+
+						_, err := vimrc_file.Seek(0, 0)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail change file offset.\n")
+							os.Exit(ExitCodeError)
+						}
+
+						vimrc_content, err := createAddPluginContentForNeoBundle(vimrc_file, plugin_name)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+							os.Exit(ExitCodeError)
+						}
+						if err := updateVimrc(conf.VimrcPath, vimrc_content); err != nil {
 							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail add plugin.\n")
 							os.Exit(ExitCodeError)
 						}
 					case "dein.vim":
+						scanAddLineForDein(vimrc_file)
+
+						_, err := vimrc_file.Seek(0, 0)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail change file offset.\n")
+							os.Exit(ExitCodeError)
+						}
+
 						vimrc_content, err := createAddPluginContentForDein(vimrc_file, plugin_name)
 						if err != nil {
 							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
@@ -136,6 +342,7 @@ func makeApp() *cli.App {
 				}
 
 				fmt.Printf("\x1b[32m%s\x1b[0m", "Success: Add plugin.\n")
+
 				return nil
 			},
 		},
@@ -145,7 +352,7 @@ func makeApp() *cli.App {
 			Usage:   "remove plugin",
 			Action: func(c *cli.Context) error {
 				// 設定ファイルの読み込み
-				var plugin_name = c.Args().First()
+				plugin_name = c.Args().First()
 				var conf Config
 				if _, err := toml.DecodeFile(setting_file_path, &conf); err != nil {
 					fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read setting file.\n")
@@ -163,6 +370,14 @@ func makeApp() *cli.App {
 					// true: プラグインマネージャーの種類を取得し、case文でそれぞれ処理
 					switch conf.ManagerType {
 					case "Vundle":
+						scanRemoveLineForVundle(vimrc_file)
+
+						_, err := vimrc_file.Seek(0, 0)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail change file offset.\n")
+							os.Exit(ExitCodeError)
+						}
+
 						vimrc_content, err := createRemovePluginContentForVundle(vimrc_file, plugin_name)
 						if err != nil {
 							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
@@ -173,6 +388,14 @@ func makeApp() *cli.App {
 							os.Exit(ExitCodeError)
 						}
 					case "NeoBundle":
+						scanRemoveLineForNeoBundle(vimrc_file)
+
+						_, err := vimrc_file.Seek(0, 0)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail change file offset.\n")
+							os.Exit(ExitCodeError)
+						}
+
 						vimrc_content, err := createRemovePluginContentForNeoBundle(vimrc_file, plugin_name)
 						if err != nil {
 							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
@@ -183,6 +406,14 @@ func makeApp() *cli.App {
 							os.Exit(ExitCodeError)
 						}
 					case "dein.vim":
+						scanRemoveLineForDein(vimrc_file)
+
+						_, err := vimrc_file.Seek(0, 0)
+						if err != nil {
+							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail change file offset.\n")
+							os.Exit(ExitCodeError)
+						}
+
 						vimrc_content, err := createRemovePluginContentForDein(vimrc_file, plugin_name)
 						if err != nil {
 							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
@@ -225,26 +456,18 @@ func makeApp() *cli.App {
 						os.Exit(ExitCodeError)
 					}
 					defer vimrc_file.Close()
+
 					// true: プラグインマネージャーの種類を取得し、case文でそれぞれ処理
 					switch conf.ManagerType {
 					case "Vundle":
-						err := listPluginForVundle(vimrc_file)
-						if err != nil {
-							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
-							os.Exit(ExitCodeError)
-						}
+						scanListPluginForVundle(vimrc_file)
+						listPlugin()
 					case "NeoBundle":
-						err := listPluginForNeoBundle(vimrc_file)
-						if err != nil {
-							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
-							os.Exit(ExitCodeError)
-						}
+						scanListPluginForNeoBundle(vimrc_file)
+						listPlugin()
 					case "dein.vim":
-						err := listPluginForDein(vimrc_file)
-						if err != nil {
-							fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
-							os.Exit(ExitCodeError)
-						}
+						scanListPluginForDein(vimrc_file)
+						listPlugin()
 					default:
 						fmt.Printf("\x1b[31m%s\x1b[0m", "Error: ManagerType is not specified.\n")
 						os.Exit(ExitCodeError)
@@ -275,23 +498,165 @@ func fileExists(filename string) bool {
 	return err == nil
 }
 
-func addPluginForVundle(vimrc_file *os.File, plugin_name string) error {
-	writer := bufio.NewWriter(vimrc_file)
-	_, err := writer.WriteString("\nBundle '" + plugin_name + "'")
-	writer.Flush()
-	return err
+func scanAddLineForVundle(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
+		os.Exit(ExitCodeError)
+	}
+	v := new(AddVundleVisitor)
+	ast.Walk(v, f)
+
+	return
+}
+
+func scanAddLineForNeoBundle(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
+		os.Exit(ExitCodeError)
+	}
+	v := new(AddNeoBundleVisitor)
+	ast.Walk(v, f)
+
+	return
+}
+
+func scanAddLineForDein(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
+		os.Exit(ExitCodeError)
+	}
+	v := new(AddDeinVisitor)
+	ast.Walk(v, f)
+
+	return
+}
+
+func createAddPluginContentForVundle(vimrc_file *os.File, plugin_name string) ([]byte, error) {
+	var rows []string
+	var index int = 1
+	scanner := bufio.NewScanner(vimrc_file)
+	for scanner.Scan() {
+		var scan_text = scanner.Text()
+		rows = append(rows, scan_text)
+		if addLine == index {
+			rows = append(rows, "Bundle '"+plugin_name+"'")
+		}
+		index++
+	}
+	if addLine == 0 {
+		rows = append(rows, "Bundle '"+plugin_name+"'")
+	}
+	vimrc_content := []byte(strings.Join(rows, "\n"))
+
+	err := scanner.Err()
+	return vimrc_content, err
+}
+
+func createAddPluginContentForNeoBundle(vimrc_file *os.File, plugin_name string) ([]byte, error) {
+	var rows []string
+	var index int = 1
+	scanner := bufio.NewScanner(vimrc_file)
+	for scanner.Scan() {
+		var scan_text = scanner.Text()
+		rows = append(rows, scan_text)
+		if addLine == index {
+			rows = append(rows, "NeoBundle '"+plugin_name+"'")
+		}
+		index++
+	}
+	if addLine == 0 {
+		rows = append(rows, "NeoBundle '"+plugin_name+"'")
+	}
+	vimrc_content := []byte(strings.Join(rows, "\n"))
+
+	err := scanner.Err()
+	return vimrc_content, err
+}
+
+func createAddPluginContentForDein(vimrc_file *os.File, plugin_name string) ([]byte, error) {
+	var rows []string
+	var index int = 1
+	scanner := bufio.NewScanner(vimrc_file)
+	for scanner.Scan() {
+		var scan_text = scanner.Text()
+		rows = append(rows, scan_text)
+		if addLine == index {
+			rows = append(rows, "call dein#add('"+plugin_name+"')")
+		}
+		index++
+	}
+	if addLine == 0 {
+		rows = append(rows, "call dein#add('"+plugin_name+"')")
+	}
+	vimrc_content := []byte(strings.Join(rows, "\n"))
+
+	err := scanner.Err()
+	return vimrc_content, err
+}
+
+func scanRemoveLineForVundle(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
+		os.Exit(ExitCodeError)
+	}
+	v := new(RemoveVundleVisitor)
+	ast.Walk(v, f)
+
+	return
 }
 
 func createRemovePluginContentForVundle(vimrc_file *os.File, plugin_name string) ([]byte, error) {
 	var rows []string
+	var index int = 1
 	scanner := bufio.NewScanner(vimrc_file)
 	for scanner.Scan() {
 		var scan_text = scanner.Text()
-		if strings.Contains(scan_text, "Bundle '"+plugin_name+"'") {
+		if index == removeLine {
+			index++
 			continue
 		} else {
 			rows = append(rows, scan_text)
 		}
+		index++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+		os.Exit(ExitCodeError)
+	}
+	vimrc_content := []byte(strings.Join(rows, "\n"))
+	err := scanner.Err()
+	return vimrc_content, err
+}
+
+func scanRemoveLineForNeoBundle(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
+		os.Exit(ExitCodeError)
+	}
+	v := new(RemoveNeoBundleVisitor)
+	ast.Walk(v, f)
+
+	return
+}
+
+func createRemovePluginContentForNeoBundle(vimrc_file *os.File, plugin_name string) ([]byte, error) {
+	var rows []string
+	var index int = 1
+	scanner := bufio.NewScanner(vimrc_file)
+	for scanner.Scan() {
+		var scan_text = scanner.Text()
+		if index == removeLine {
+			index++
+			continue
+		} else {
+			rows = append(rows, scan_text)
+		}
+		index++
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
@@ -309,56 +674,31 @@ func addPluginForNeoBundle(vimrc_file *os.File, plugin_name string) error {
 	return err
 }
 
-func createRemovePluginContentForNeoBundle(vimrc_file *os.File, plugin_name string) ([]byte, error) {
-	var rows []string
-	scanner := bufio.NewScanner(vimrc_file)
-	for scanner.Scan() {
-		var scan_text = scanner.Text()
-		rows = append(rows, scan_text)
-		if strings.Contains(scan_text, "NeoBundle '"+plugin_name+"'") {
-			continue
-		} else {
-			rows = append(rows, scan_text)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+func scanRemoveLineForDein(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
 		os.Exit(ExitCodeError)
 	}
-	vimrc_content := []byte(strings.Join(rows, "\n"))
-	err := scanner.Err()
-	return vimrc_content, err
-}
+	v := new(RemoveDeinVisitor)
+	ast.Walk(v, f)
 
-func createAddPluginContentForDein(vimrc_file *os.File, plugin_name string) ([]byte, error) {
-	var rows []string
-	scanner := bufio.NewScanner(vimrc_file)
-	for scanner.Scan() {
-		var scan_text = scanner.Text()
-		rows = append(rows, scan_text)
-		if strings.Contains(scan_text, "call dein#begin") {
-			rows = append(rows, "call dein#add('"+plugin_name+"')")
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
-		os.Exit(ExitCodeError)
-	}
-	vimrc_content := []byte(strings.Join(rows, "\n"))
-	err := scanner.Err()
-	return vimrc_content, err
+	return
 }
 
 func createRemovePluginContentForDein(vimrc_file *os.File, plugin_name string) ([]byte, error) {
 	var rows []string
+	var index int = 1
 	scanner := bufio.NewScanner(vimrc_file)
 	for scanner.Scan() {
 		var scan_text = scanner.Text()
-		if strings.Contains(scan_text, "call dein#add('"+plugin_name+"')") {
+		if index == removeLine {
+			index++
 			continue
 		} else {
 			rows = append(rows, scan_text)
 		}
+		index++
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
@@ -381,59 +721,49 @@ func updateVimrc(vimrc_file_path string, vimrc_content []byte) error {
 	return err
 }
 
-func listPluginForVundle(vimrc_file *os.File) error {
-	scanner := bufio.NewScanner(vimrc_file)
-	for scanner.Scan() {
-		var scan_text = scanner.Text()
-		if strings.Contains(scan_text, "Bundle '") {
-			scan_text = strings.Replace(scan_text, "Bundle", "", 1)
-			scan_text = strings.Replace(scan_text, "'", "", -1)
-			println(scan_text)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+func scanListPluginForVundle(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
 		os.Exit(ExitCodeError)
 	}
-	err := scanner.Err()
-	return err
+	v := new(ListVundleVisitor)
+	ast.Walk(v, f)
+
+	return
 }
 
-func listPluginForNeoBundle(vimrc_file *os.File) error {
-	scanner := bufio.NewScanner(vimrc_file)
-	for scanner.Scan() {
-		var scan_text = scanner.Text()
-		if strings.Contains(scan_text, "NeoBundle '") {
-			scan_text = strings.Replace(scan_text, "NeoBundle", "", 1)
-			scan_text = strings.Replace(scan_text, "'", "", -1)
-			println(scan_text)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+func scanListPluginForNeoBundle(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
 		os.Exit(ExitCodeError)
 	}
-	err := scanner.Err()
-	return err
+	v := new(ListNeoBundleVisitor)
+	ast.Walk(v, f)
+
+	return
 }
 
-func listPluginForDein(vimrc_file *os.File) error {
-	scanner := bufio.NewScanner(vimrc_file)
-	for scanner.Scan() {
-		var scan_text = scanner.Text()
-		if strings.Contains(scan_text, "call dein#add") {
-			scan_text = strings.Replace(scan_text, "call dein#add", "", 1)
-			scan_text = strings.Replace(scan_text, "('", "", 1)
-			scan_text = strings.Replace(scan_text, "')", "", 1)
-			println(scan_text)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Can't read .vimrc file.\n")
+func scanListPluginForDein(vimrc_file *os.File) {
+	f, err := vimlparser.ParseFile(vimrc_file, "", opt)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Error: Fail parse .vimrc file.\n")
 		os.Exit(ExitCodeError)
 	}
-	err := scanner.Err()
-	return err
+	v := new(ListDeinVisitor)
+	ast.Walk(v, f)
+
+	return
+}
+
+func listPlugin() {
+	if len(installPlugins) == 0 {
+		fmt.Printf("\x1b[31m%s\x1b[0m", "Nothing install plugin.\n")
+	}
+	for _, install_plugin := range installPlugins {
+		println(install_plugin)
+	}
 }
 
 func main() {
