79fc6d6d8ff0732136fbfb28fecb85c57e0725cc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-01-21 02:48:30 +0900

    add missing tests

--- kirimori_test.go
+++ kirimori_test.go
@@ -4,10 +4,11 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
-func testPlugin(t *testing.T, name, key string) {
+func testAddPlugin(t *testing.T, name, key string) {
 	settingFilePath = filepath.Join("testdir", key+".toml")
 	conf := config()
 	if conf.ManagerType != name {
@@ -20,6 +21,7 @@ func testPlugin(t *testing.T, name, key string) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer f.Close()
 	line := conf.Manager().AddLine(f)
 
 	_, err = f.Seek(0, 0)
@@ -42,8 +44,69 @@ func testPlugin(t *testing.T, name, key string) {
 	}
 }
 
+func testListPlugin(t *testing.T, name, key string) {
+	settingFilePath = filepath.Join("testdir", key+".toml")
+	conf := config()
+	if conf.ManagerType != name {
+		t.Fatalf("expected %v but %v", name, conf.ManagerType)
+	}
+
+	f, err := os.Open(conf.VimrcPath + ".add")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	plugins := conf.Manager().ListPlugin(f)
+
+	fb, err := ioutil.ReadFile(filepath.Join("testdir", key+".list"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := strings.TrimSpace(string(fb))
+	got := strings.Join(plugins, "\n")
+	if expected != got {
+		t.Fatalf("expected %s but %s", expected, got)
+	}
+}
+
+func testRemovePlugin(t *testing.T, name, key string) {
+	settingFilePath = filepath.Join("testdir", key+".toml")
+	conf := config()
+	if conf.ManagerType != name {
+		t.Fatalf("expected %v but %v", name, conf.ManagerType)
+	}
+
+	f, err := os.Open(conf.VimrcPath + ".add")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	line := conf.Manager().RemoveLine(f, "mattn/emmet-vim")
+
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := createRemovePluginContent(f, line)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fb, err := ioutil.ReadFile(filepath.Join("testdir", key+".vimrc"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := string(fb)
+	got := string(b)
+	if expected != got {
+		t.Fatalf("expected %s but %s", expected, got)
+	}
+}
+
 func TestPlugins(t *testing.T) {
 	for _, pm := range pluginManagers {
-		testPlugin(t, pm.Name, pm.Key)
+		testAddPlugin(t, pm.Name, pm.Key)
+		testListPlugin(t, pm.Name, pm.Key)
+		testRemovePlugin(t, pm.Name, pm.Key)
 	}
 }
